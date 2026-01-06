7124f87f06c4c6e9c59030347c4487b8ff05e751
lnotspotl <lnotspotl@gmail.com>
2026-01-06 19:33:09 +0100

    add franka mpc

---
+++ tbai_ros_mpc/include/tbai_ros_mpc/franka_mpc/MobileManipulatorDummyVisualization.h
@@ -0,0 +1,71 @@
+/******************************************************************************
+Copyright (c) 2020, Farbod Farshidian. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+ * Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+#pragma once
+
+#include <robot_state_publisher/robot_state_publisher.h>
+#include <tf/transform_broadcaster.h>
+#include <ros/ros.h>
+
+#include <ocs2_mpc/SystemObservation.h>
+#include <ocs2_oc/oc_data/PrimalSolution.h>
+#include <ocs2_mpc/CommandData.h>
+
+#include <tbai_mpc/franka_mpc/ManipulatorModelInfo.h>
+#include <tbai_mpc/franka_mpc/MobileManipulatorInterface.h>
+
+namespace ocs2 {
+namespace mobile_manipulator {
+
+class MobileManipulatorDummyVisualization {
+ public:
+  MobileManipulatorDummyVisualization(ros::NodeHandle& nodeHandle, const MobileManipulatorInterface& interface);
+  ~MobileManipulatorDummyVisualization() = default;
+
+  void update(const SystemObservation& observation, const PrimalSolution& policy, const CommandData& command);
+
+ private:
+  void launchVisualizerNode(ros::NodeHandle& nodeHandle);
+  void publishObservation(const ros::Time& timeStamp, const SystemObservation& observation);
+  void publishTargetTrajectories(const ros::Time& timeStamp, const TargetTrajectories& targetTrajectories);
+  void publishOptimizedTrajectory(const ros::Time& timeStamp, const PrimalSolution& policy);
+
+  PinocchioInterface pinocchioInterface_;
+  const ManipulatorModelInfo modelInfo_;
+  std::vector<std::string> removeJointNames_;
+
+  std::unique_ptr<robot_state_publisher::RobotStatePublisher> robotStatePublisherPtr_;
+  tf::TransformBroadcaster tfBroadcaster_;
+
+  ros::Publisher stateOptimizedPublisher_;
+  ros::Publisher stateOptimizedPosePublisher_;
+};
+
+}  // namespace mobile_manipulator
+}  // namespace ocs2
---
+++ tbai_ros_mpc/src/franka_mpc/MobileManipulatorTarget.cpp
@@ -0,0 +1,179 @@
+/******************************************************************************
+Copyright (c) 2017, Farbod Farshidian. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+ * Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+#include <ros/ros.h>
+#include <interactive_markers/interactive_marker_server.h>
+
+#include <ocs2_core/Types.h>
+#include <ocs2_core/reference/TargetTrajectories.h>
+#include <ocs2_mpc/SystemObservation.h>
+
+#include <tbai_ros_ocs2/common/RosMsgConversions.h>
+#include <tbai_ros_ocs2/mpc_observation.h>
+#include <tbai_ros_ocs2/mpc_target_trajectories.h>
+
+using namespace ocs2;
+
+class TargetTrajectoriesPublisher {
+ public:
+  TargetTrajectoriesPublisher(ros::NodeHandle& nodeHandle, const std::string& topicPrefix)
+      : server_("target_marker") {
+    // Publisher for target trajectories
+    targetPublisher_ = nodeHandle.advertise<tbai_ros_ocs2::mpc_target_trajectories>(topicPrefix + "_mpc_target", 1);
+
+    // Subscriber for observation (to get current time)
+    observationSubscriber_ = nodeHandle.subscribe(topicPrefix + "_mpc_observation", 1,
+                                                   &TargetTrajectoriesPublisher::observationCallback, this);
+
+    // Create interactive marker
+    createInteractiveMarker();
+  }
+
+  void createInteractiveMarker() {
+    visualization_msgs::InteractiveMarker intMarker;
+    intMarker.header.frame_id = "panda_link0";
+    intMarker.header.stamp = ros::Time::now();
+    intMarker.name = "ee_target";
+    intMarker.description = "End Effector Target";
+    intMarker.scale = 0.2;
+
+    // Initial position
+    intMarker.pose.position.x = 0.5;
+    intMarker.pose.position.y = 0.0;
+    intMarker.pose.position.z = 0.5;
+    intMarker.pose.orientation.w = 1.0;
+
+    // Create a sphere marker for visual feedback
+    visualization_msgs::Marker sphereMarker;
+    sphereMarker.type = visualization_msgs::Marker::SPHERE;
+    sphereMarker.scale.x = 0.08;
+    sphereMarker.scale.y = 0.08;
+    sphereMarker.scale.z = 0.08;
+    sphereMarker.color.r = 1.0;
+    sphereMarker.color.g = 0.5;
+    sphereMarker.color.b = 0.0;
+    sphereMarker.color.a = 0.8;
+
+    visualization_msgs::InteractiveMarkerControl sphereControl;
+    sphereControl.always_visible = true;
+    sphereControl.markers.push_back(sphereMarker);
+    intMarker.controls.push_back(sphereControl);
+
+    // Add 6DOF controls
+    visualization_msgs::InteractiveMarkerControl control;
+
+    control.orientation.w = 1;
+    control.orientation.x = 1;
+    control.orientation.y = 0;
+    control.orientation.z = 0;
+    control.name = "rotate_x";
+    control.interaction_mode = visualization_msgs::InteractiveMarkerControl::ROTATE_AXIS;
+    intMarker.controls.push_back(control);
+    control.name = "move_x";
+    control.interaction_mode = visualization_msgs::InteractiveMarkerControl::MOVE_AXIS;
+    intMarker.controls.push_back(control);
+
+    control.orientation.w = 1;
+    control.orientation.x = 0;
+    control.orientation.y = 1;
+    control.orientation.z = 0;
+    control.name = "rotate_y";
+    control.interaction_mode = visualization_msgs::InteractiveMarkerControl::ROTATE_AXIS;
+    intMarker.controls.push_back(control);
+    control.name = "move_y";
+    control.interaction_mode = visualization_msgs::InteractiveMarkerControl::MOVE_AXIS;
+    intMarker.controls.push_back(control);
+
+    control.orientation.w = 1;
+    control.orientation.x = 0;
+    control.orientation.y = 0;
+    control.orientation.z = 1;
+    control.name = "rotate_z";
+    control.interaction_mode = visualization_msgs::InteractiveMarkerControl::ROTATE_AXIS;
+    intMarker.controls.push_back(control);
+    control.name = "move_z";
+    control.interaction_mode = visualization_msgs::InteractiveMarkerControl::MOVE_AXIS;
+    intMarker.controls.push_back(control);
+
+    server_.insert(intMarker, boost::bind(&TargetTrajectoriesPublisher::markerCallback, this, _1));
+    server_.applyChanges();
+  }
+
+  void markerCallback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr& feedback) {
+    if (feedback->event_type == visualization_msgs::InteractiveMarkerFeedback::POSE_UPDATE) {
+      // Extract position and orientation
+      Eigen::Vector3d position(feedback->pose.position.x,
+                               feedback->pose.position.y,
+                               feedback->pose.position.z);
+      Eigen::Quaterniond orientation(feedback->pose.orientation.w,
+                                      feedback->pose.orientation.x,
+                                      feedback->pose.orientation.y,
+                                      feedback->pose.orientation.z);
+
+      // Create target state (7D: position + quaternion)
+      vector_t target(7);
+      target.head(3) = position;
+      target.tail(4) = orientation.coeffs();
+
+      // Create zero input (will be inferred by MPC)
+      vector_t zeroInput = vector_t::Zero(7);  // 7 DOF arm
+
+      // Create target trajectories
+      TargetTrajectories targetTrajectories({latestTime_}, {target}, {zeroInput});
+
+      // Publish
+      auto msg = ros_msg_conversions::createTargetTrajectoriesMsg(targetTrajectories);
+      targetPublisher_.publish(msg);
+    }
+  }
+
+  void observationCallback(const tbai_ros_ocs2::mpc_observation::ConstPtr& msg) {
+    latestTime_ = msg->time;
+  }
+
+ private:
+  interactive_markers::InteractiveMarkerServer server_;
+  ros::Publisher targetPublisher_;
+  ros::Subscriber observationSubscriber_;
+  scalar_t latestTime_ = 0.0;
+};
+
+int main(int argc, char* argv[]) {
+  const std::string robotName = "mobile_manipulator";
+  ros::init(argc, argv, robotName + "_target");
+  ros::NodeHandle nodeHandle;
+
+  TargetTrajectoriesPublisher targetPublisher(nodeHandle, robotName);
+
+  ROS_INFO("Mobile Manipulator Target: Interactive marker ready. Move the marker in RViz to set targets.");
+
+  ros::spin();
+
+  return 0;
+}
